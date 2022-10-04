ad54074824f2861bc04580e5a1e771fa1d60fe6b
Jayce <thainguyenhoangphatit@gmail.com>
2022-10-04 20:02:52 +0700

    Updates

--- static/js/122.2ab7150d.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkpoc_app=self.webpackChunkpoc_app||[]).push([[122],{2122:function(t,a,e){e.r(a),e.d(a,{default:function(){return i}});var n=e(3433),u=e(3286),o=e(2791),r=e(184),c=function(){return(0,r.jsx)("div",{className:"w-fit m-auto",children:(0,r.jsx)(u.Z,{data:{size:200,pieData:(0,n.Z)(Array(10)).map((function(t,a){return{id:a,label:"Label ".concat(a),value:Math.floor(1e4*Math.random()+1)}})),donutText:"Donut text",subDonutText:"Sub donut text"}})})},i=o.memo(c)}}]);
-//# sourceMappingURL=122.2ab7150d.chunk.js.map
